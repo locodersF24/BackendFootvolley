083ec1069f6e8dc5273792efdd82225e59445b93
esralund01 <calu001@stud.kea.dk>
2025-05-08 09:57:27 +0200

    test annotation

--- src/test/java/org/example/backendfootvolley/BackendFootvolleyApplicationTests.java
+++ src/test/java/org/example/backendfootvolley/BackendFootvolleyApplicationTests.java
@@ -2,9 +2,11 @@ package org.example.backendfootvolley;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendFootvolleyApplicationTests {
 
