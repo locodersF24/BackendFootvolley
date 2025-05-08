a7d7bdefec2a53fba9f865f939892d9934782a2d
esralund01 <calu001@stud.kea.dk>
2025-05-08 09:45:01 +0200

    basic test

--- src/test/java/org/example/backendfootvolley/BackendFootvolleyApplicationTests.java
+++ src/test/java/org/example/backendfootvolley/BackendFootvolleyApplicationTests.java
@@ -3,11 +3,14 @@ package org.example.backendfootvolley;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class BackendFootvolleyApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
